30ed7b08bca5975523c32fb1a08b3d12ca049b48
britto vincent <brittovincent111@gmail.com>
2023-10-02 16:13:36 +0530

    visa issue

--- src/features/Visa/components/VisaApplicationTable.jsx
+++ src/features/Visa/components/VisaApplicationTable.jsx
@@ -9,28 +9,30 @@ import { avatarImg } from "../../../assets/images";
 import axios from "../../../axios";
 
 export default function VisaApplicationTable({ visaApplication, orderedBy }) {
-  const [status, setStatus] = useState(visaApplication?.status);
-  const [isStatusLoading, setIsStatusLoading] = useState(false);
+    const [status, setStatus] = useState(visaApplication?.status);
+    const [isStatusLoading, setIsStatusLoading] = useState(false);
 
-  const navigate = useNavigate();
-  const { jwtToken } = useSelector((state) => state.admin);
+    const navigate = useNavigate();
+    const { jwtToken } = useSelector((state) => state.admin);
 
-  return (
-    <tr
-      className="border-b border-tableBorderColor transition-all cursor-pointer hover:bg-[#f3f6f9]"
-      onClick={() =>
-        navigate(
-          `/visa/${orderedBy}/${visaApplication?._id}/application/${visaApplication.travellers._id}`
-        )
-      }
-    >
-      <td className="p-3">{visaApplication?.referenceNumber}</td>
-      <td className="p-3">
-        <span className="block">{visaApplication?.visaType}</span>
-        <span className="text-grayColor block">{visaApplication?.visa}</span>
-      </td>
+    return (
+        <tr
+            className="border-b border-tableBorderColor transition-all cursor-pointer hover:bg-[#f3f6f9]"
+            onClick={() =>
+                navigate(
+                    `/visa/${orderedBy}/${visaApplication?._id}/application/${visaApplication.travellers._id}`
+                )
+            }
+        >
+            <td className="p-3">{visaApplication?.referenceNumber}</td>
+            <td className="p-3">
+                <span className="block">{visaApplication?.visaType}</span>
+                <span className="text-grayColor block">
+                    {visaApplication?.visa}
+                </span>
+            </td>
 
-      {/* <td className="p-3">
+            {/* <td className="p-3">
                 <div className="flex items-center gap-[10px]">
                     <img
                         src={
@@ -50,61 +52,52 @@ export default function VisaApplicationTable({ visaApplication, orderedBy }) {
                     </div>
                 </div>
             </td> */}
-      {orderedBy == "b2c" ? (
-        <td className="p-3">
-          <span className="block">{visaApplication?.user?.name} </span>
-          <span className="text-grayColor block">
-            {visaApplication?.user?.email}
-          </span>
-        </td>
-      ) : (
-        <td className="p-3">
-          <span className="block">{visaApplication?.reseller?.name} </span>
-          <span className="text-grayColor block">
-            {visaApplication?.reseller?.email}
-          </span>
-        </td>
-      )}
+            {orderedBy == "b2c" ? (
+                <td className="p-3">
+                    <span className="block">
+                        {visaApplication?.user?.name}{" "}
+                    </span>
+                    <span className="text-grayColor block">
+                        {visaApplication?.user?.email}
+                    </span>
+                </td>
+            ) : (
+                <td className="p-3">
+                    <span className="block">
+                        {visaApplication?.reseller?.name}{" "}
+                    </span>
+                    <span className="text-grayColor block">
+                        {visaApplication?.reseller?.email}
+                    </span>
+                </td>
+            )}
 
-      <td className="p-3">
-        <span className="block">
-          {visaApplication?.travellers?.firstName}{" "}
-          {visaApplication?.travellers?.lastName}{" "}
-        </span>
-        <span className="text-grayColor block">
-          {visaApplication?.travellers?.email}
-        </span>
-      </td>
-      {/* <td className="p-3 capitalize">{visaApplication?.visaType.visa.country?.countryName}</td> */}
-      <td className="p-3">{formatDate(visaApplication?.createdAt)}</td>
-      <td className="p-3">
-        { visaApplication?.travellers?.isStatus === "approved" ? (
-          <div>
-            <span className="py-1 px-2 text-[12px] font-medium rounded text-[#008000] bg-[#f065481a]">
-              Approved
-            </span>
-          </div>
-        ) : visaApplication?.travellers?.isStatus === "rejected" ? (
-          <div>
-            <span className="py-1 px-2 text-[12px] font-medium rounded text-[#ff0000] bg-[#f065481a]">
-              Rejected
-            </span>
-          </div>
-        ) : 
-        visaApplication?.travellers?.isStatus === "submitted" ? (
-          <div>
-            <span className="py-1 px-2 text-[12px] font-medium rounded text-[#f06548] bg-[#f065481a]">
-              Submitted
-            </span>
-          </div>
-        ) :(
-          <div>
-            <span className="py-1 px-2 text-[12px] font-medium rounded  bg-[#f065481a]">
-              Initiated
-            </span>
-          </div>
-        )}
-      </td>
-    </tr>
-  );
+            <td className="p-3">
+                <span className="block">
+                    {visaApplication?.travellers?.firstName}{" "}
+                    {visaApplication?.travellers?.lastName}{" "}
+                </span>
+                <span className="text-grayColor block">
+                    {visaApplication?.travellers?.email}
+                </span>
+            </td>
+            {/* <td className="p-3 capitalize">{visaApplication?.visaType.visa.country?.countryName}</td> */}
+            <td className="p-3">{formatDate(visaApplication?.createdAt)}</td>
+            <td className="p-3">
+                {visaApplication?.status === "payed" ? (
+                    <div>
+                        <span className="py-1 px-2 text-[12px] font-medium rounded text-[#008000] bg-[#f065481a]">
+                            Paid
+                        </span>
+                    </div>
+                ) : (
+                    <div>
+                        <span className="py-1 px-2 text-[12px] font-medium rounded text-[#ff0000] bg-[#f065481a]">
+                            Initated
+                        </span>
+                    </div>
+                )}
+            </td>
+        </tr>
+    );
 }
